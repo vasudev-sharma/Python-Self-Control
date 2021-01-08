037d05d1e72c91b9d0c038b424fce443983726df
vs74 <vasudevsharma74@yahoo.com>
2021-01-08 21:47:22 +0530

    Add websites names with help of CLI through -l argument

--- script.py
+++ script.py
@@ -1,42 +1,71 @@
-from datetime import datetime
+from datetime import datetime, date
+import logging
+import os
+
+import argparse
 
 
-redirect = "127.0.0.1" 
-string_endtime = map(int, input("Enter the end time in the format YYYY, MM, DD, HH: - ").split(','))
-YYYY, MM, DD, HH = string_endtime
-print(type(YYYY), type(MM), type(DD), type(HH) )
-endtime = datetime(YYYY, MM, DD, HH)
-print(endtime)
+def self_control(list_sites):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    file_name = os.path.join(dir_path, "test_log.log")
 
-sites_to_block = ["youtube.com", "www.youtube.com", "facebook.com", "www.facebook.com", "gradcafe.com", "www.gradcafe.com", "voot.com", "www.voot.com"]
-reply = input("Do you wish to enter more websites to block: Y/N -  ")
-if reply == "Y":
-    while(True):
-        site = input("Enter the site name: - ")
-        sites_to_block.append(site)
-        sites_to_block.append("www."+site)
-        reply = input("Do you wish to enter more websites to block: Y/N -  ")
-        if reply == "N":
-            break
 
-#The list of sites to block 
-print(sites_to_block)
+    #logger to log the files
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.INFO)
+    
 
-if datetime.now() < endtime:
-    with open('/etc/hosts', "r+") as file:
-        file_content = file.read()
-        print(file_content)
-        for site_name in sites_to_block:
-            if site_name not in file_content:
-                file.write(redirect + " "+ site_name + "\n")
 
-else:
-    with open('/etc/hosts', "r+") as file:
-        file_content = file.readlines()
-        for site_name in sites_to_block:
-            if (redirect + " " + site_name + '\n') in file_content:
-                file_content.remove(redirect + " " + site_name + '\n')
-        file.truncate(0)
-        for content in file_content: 
-          file.write(content)  
-      
+    #create a file handler
+
+    file_handler = logging.FileHandler(file_name)
+    file_handler.setLevel(logging.INFO)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s - %(levelname)s - %(message)s'))
+    logger.addHandler(file_handler)
+
+    #reference  - 2021, 01, 04, 19 
+
+    redirect = "127.0.0.1" 
+    endtime =  list(map(int, str(date.today()).split("-")))
+
+
+    YYYY, MM, DD = endtime
+    HH = 17
+    endtime = datetime(YYYY, MM, DD, HH)
+    sites_to_block = list_sites.split(",")
+    print(sites_to_block)
+    
+    #sites_to_block = ["youtube.com, www.youtube.com, facebook.com, www.facebook.com, gradcafe.com, www.gradcafe.com, voot.com, www.voot.com, twitter.com, www.twitter.com, gmail.com, www.gmail.com, yahoo.com, www.yahoo.com"]
+  
+    #The list of sites to block 
+    print(sites_to_block)
+
+    if datetime.now() < endtime:
+        with open('/etc/hosts', "r+") as file:
+            file_content = file.read()
+            for site_name in sites_to_block:
+                if site_name not in file_content:
+                    file.write(redirect + " "+ site_name + "\n")
+            #print(file.read())
+        logger.info("SITES BLOCKED")
+
+    else:
+        with open('/etc/hosts', "r+") as file:
+            file_content = file.readlines()
+            for site_name in sites_to_block:
+                if (redirect + " " + site_name + '\n') in file_content:
+                    file_content.remove(redirect + " " + site_name + '\n')
+            file.truncate(0)
+            print(file_content)
+            for content in file_content: 
+                file.write(content)  
+            print(file.readlines())
+        logger.info("SITES UNBLOCKED")  
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-l', required = True,
+                    help='Enter the site names separated by comma')
+    args = parser.parse_args()
+
+    self_control(args.l)
